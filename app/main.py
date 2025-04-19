d61ab35d051085dda20c41731a77dbfa9c192908
hitesharora1997 <hitesh.arora1997@gmail.com>
2025-04-20 00:49:03 +0200

    feat: add Task schema and in-memory task endpoints

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
-# app/main.py
-
 from fastapi import FastAPI
+from app.api.task_router import router as task_router
 
 app = FastAPI()
+
+app.include_router(task_router)
 
 @app.get("/")
 def read_root():
